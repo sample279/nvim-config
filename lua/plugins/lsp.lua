f02e2d3f6b346d8769ad0a7ca5c7dfc9d44b0b53
sample279 <224307167+sample279@users.noreply.github.com>
2026-03-29 17:34:16 +0530

    add lsp for json files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,27 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    opts = {
-      diagnostics = {
+    dependencies = {
+      "b0o/schemastore.nvim",
+    },
+    opts = function(_, opts)
+      opts.diagnostics = {
         virtual_text = false,
         float = {
           border = "rounded",
         },
-      },
-    },
+      }
+
+      opts.servers = opts.servers or {}
+
+      opts.servers.jsonls = {
+        settings = {
+          json = {
+            schemas = require("schemastore").json.schemas(),
+            validate = { enable = true },
+          },
+        },
+      }
+    end,
   },
 }
